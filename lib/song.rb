528359849d5e32b907f6fec33497ae674c3a11db
Richard Cho <96richardohc@gmail.com>
2020-02-13 07:26:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  attr_accessor :name, :artist, :genre
+
+def initialize(name, artist, genre)
+  @name=name
+  @artist=artist
+  @genre=genre
+  @@count += 1
+  @@artists << artist
+  @@genres << genre
+end
+
+def self.count
+  @@count
+end
+
+def self.genres
+  @@genres.uniq!
+end
+
+def self.artists 
+  @@artists.uniq!
+end
+
+def genre_count
+  genres.each do |genre, count|
+      genres[genre] = count.sort
+    end
+    end
+
+end
