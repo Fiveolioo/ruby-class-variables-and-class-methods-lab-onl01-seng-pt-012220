2695369b1d81a6f7c0bddeccef8ba2be641dfb83
Richard Cho <96richardohc@gmail.com>
2020-02-13 07:31:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,30 +4,30 @@ class Song
   @@artists = []
   attr_accessor :name, :artist, :genre
 
-def initialize(name, artist, genre)
-  @name=name
-  @artist=artist
-  @genre=genre
-  @@count += 1
-  @@artists << artist
-  @@genres << genre
-end
+  def initialize(name, artist, genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
 
-def self.count
-  @@count
-end
+  def self.count
+    @@count
+  end
 
-def self.genres
-  @@genres.uniq!
-end
+  def self.genres
+    @@genres.uniq!
+  end
 
-def self.artists 
-  @@artists.uniq!
-end
+  def self.artists 
+    @@artists.uniq!
+  end
 
-def self.genre_count
-  # puts "one genre? #{@genre}"
-  # puts "two genre? #{@@genre}"
-end
+  def self.genre_count
+    puts "one genre? #{@genre}"
+    puts "two genre? #{@@genre}"
+  end
 
 end
